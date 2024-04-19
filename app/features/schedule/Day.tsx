37f9191c581eb565bed3efc981f26cf040cf6e21
sn0wzZz <m.stanchev1999@abv.bg>
2024-04-19 20:25:27 +0300

    Day fixes

--- app/features/schedule/Day.tsx
+++ app/features/schedule/Day.tsx
@@ -5,13 +5,13 @@ export default function Day({ day: { date, title, description } }: any) {
   return (
     <div className='max-w-[500px]'>
       <div className='flex gap-2 '>
-        <IoTodayOutline size={60} className='text-primary-100' />
-        <div>
+        <IoTodayOutline size={50} className='text-primary-100' />
+        <div className='flex flex-col gap-1'>
           <h6 className='text-textSecondary font-bold'>{date}</h6>
-          <h3 className='text-heading'>{title}</h3>
+          <h4>{title}</h4>
         </div>
       </div>
-      <p className='text-text border-l-2 pl-10 ml-7 py-8'>{description}</p>
+      <p className='text-text border-l-2 pl-8 ml-6 py-7 mt-3'>{description}</p>
     </div>
   )
 }
